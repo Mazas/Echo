97fafb009cca613693665099eaedc112ed7fe84c
Mazas <ivanauskas15@gmail.com>
2020-07-03 14:13:50 +0200

    added message handling

--- echo_client/main.py
+++ echo_client/main.py
@@ -4,6 +4,39 @@ import paho.mqtt.client as mqtt
 broker_address = "192.168.1.181"
 
 
+# Helper functions to implement each command separately
+def left_pushed():
+    return "LeftPushed"
+
+
+def left_pulled():
+    return "LeftPulled"
+
+
+def right_pushed():
+    return "RightPushed"
+
+
+def right_pulled():
+    return "RightPulled"
+
+
+def both_locked():
+    return "BothLocked"
+
+
+# Switcher method to identify the message
+def interpret_message(message):
+    switcher = {
+        "LeftPushed": left_pushed(),
+        "LeftPulled": left_pulled(),
+        "RightPushed": right_pushed(),
+        "RightPulled": right_pulled(),
+        "BothLocked": both_locked()
+    }
+    return switcher.get(message, "Invalid message")
+
+
 # connection callback
 # 0: Connection successful
 # 1: Connection refused – incorrect protocol version
@@ -22,12 +55,12 @@ def on_connect(client, userdata, flags, rc):
 
 
 # Callback to process the message
-# TODO edit to actually process the message
 def on_message(client, userdata, message):
     received = str(message.payload.decode("utf-8"))
-    print("Message received: ", received)
+    print("Message received: ", interpret_message(received))
 
 
+# main function
 def main():
     print("Connecting...", end="")
     # Create client
